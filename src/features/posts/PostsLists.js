fcf4d7a5a42a00c4135fcdbaeb60cca9fb31fbda
codermother <ozgecoskun_3@hotmail.com>
2022-05-31 23:24:08 +0300

    Order posts

--- src/features/posts/PostsLists.js
+++ src/features/posts/PostsLists.js
@@ -7,7 +7,11 @@ import { TimeAgo } from './TimeAgo'
 export const PostsLists = () => {
   const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <PostAuthor userId={post.user} />
