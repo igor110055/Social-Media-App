845c0f1ff239ab02e3b807fa5f9e8063c39b8dfd
codermother <ozgecoskun_3@hotmail.com>
2022-06-01 15:55:07 +0300

    Create users page

---
+++ src/features/users/UsersList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { selectAllUsers } from './userSlice'
+
+const UsersList = () => {
+  const users = useSelector(selectAllUsers)
+
+  return (
+    <section>
+      <h2>Users</h2>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </section>
+  )
+}
+
+export default UsersList
